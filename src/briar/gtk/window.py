f173a6b461048f9d8c02a7b3945eabd7051d7101
Nico Alt <nicoalt@posteo.org>
2019-03-25 21:12:20 +0100

    Make window larger and center stuff

--- src/briar/gtk/window.py
+++ src/briar/gtk/window.py
@@ -37,8 +37,18 @@ class Window(Gtk.ApplicationWindow):
     def __setup_content(self):
         self.__container = Container()
         self.__container.show()
+        self.__container.set_hexpand(True)
+        self.__container.set_vexpand(True)
         self.__vgrid = Gtk.Grid()
         self.__vgrid.set_orientation(Gtk.Orientation.VERTICAL)
         self.__vgrid.show()
         self.__vgrid.add(self.__container)
         self.add(self.__vgrid)
+        self.__setup_size((800, 600))
+
+    def __setup_size(self, size):
+        if len(size) == 2 and\
+           isinstance(size[0], int) and\
+           isinstance(size[1], int):
+            self.resize(size[0], size[1])
+

660115773f69aaaffbebd872f13ad6992b6831c0
Nico Alt <nicoalt@posteo.org>
2019-06-02 00:31:58 +0200

    Remove unused imports from window

--- src/briar/gtk/window.py
+++ src/briar/gtk/window.py
@@ -9,7 +9,7 @@ from briar.gtk.define import App
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk, GLib
+from gi.repository import Gtk
 
 
 class Window(Gtk.ApplicationWindow):

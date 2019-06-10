05466aa78a504db1fd6cd122f71fd5f0fc2303ff
Nico Alt <nicoalt@posteo.org>
2019-06-10 19:15:38 +0200

    Run python linters on tests, too

--- tools/tests/test-pycodestyle.sh
+++ tools/tests/test-pycodestyle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pycodestyle --show-source --show-pep8 src
+pycodestyle --show-source --show-pep8 src tests
--- tools/tests/test-pylint.sh
+++ tools/tests/test-pylint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pylint src
+pylint src tests

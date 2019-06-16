591a8d701641f8115d8eb06f23936060226bfedc
Nico Alt <nicoalt@posteo.org>
2019-06-16 22:03:52 +0200

    Add unit test for sending messages

--- tests/briar/api/models/test_private_chat.py
+++ tests/briar/api/models/test_private_chat.py
@@ -8,6 +8,7 @@ import requests_mock
 
 from briar.api.models.private_chat import PrivateChat
 
+SEND_TEXT = "Hello World"
 
 
 @requests_mock.Mocker(kw="requests_mock")
@@ -20,3 +21,18 @@ def test_get_empty(api, request_headers, requests_mock):
     requests_mock.register_uri("GET", url, request_headers=request_headers,
                                text=json.dumps(response))
     assert private_chat.get(contact_id) == response
+
+
+@requests_mock.Mocker(kw="requests_mock")
+def test_send_message(api, request_headers, requests_mock):
+    private_chat = PrivateChat(api)
+    contact_id = 1
+    url = "http://localhost:7000/v1/messages/%s" % contact_id
+
+    requests_mock.register_uri("POST", url, request_headers=request_headers,
+                               additional_matcher=match_request_text)
+    private_chat.send(contact_id, SEND_TEXT)
+
+
+def match_request_text(request):
+    return {"text": SEND_TEXT} == request.json()

49c7ba38a15ddd7cfe68ce31881a618a5f5dbc1f
Nico Alt <nicoalt@posteo.org>
2019-12-09 01:45:34 +0100

    Simplify abstract container class

--- src/briar/gtk/container.py
+++ src/briar/gtk/container.py
@@ -2,19 +2,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # License-Filename: LICENSE.md
 
-from gi.repository import Gtk
+from gi.repository.Gtk import Builder, Overlay
 
 
-class Container(Gtk.Overlay):
+class Container(Overlay):
 
     def __init__(self):
-        Gtk.Overlay.__init__(self)
-        self.builder = Gtk.Builder()
-        self.__setup_view()
-        self.__register_signals()
-
-    def __setup_view(self):
-        pass
-
-    def __register_signals(self):
-        pass
+        super().__init__()
+        self.builder = Builder()

e5bca2d2db962349c72023d075f150ada0479095
Nico Alt <nicoalt@posteo.org>
2020-03-27 12:00:02 +0000

    Preserve Gtk prefix when importing

--- briar-gtk/briar_gtk/container.py
+++ briar-gtk/briar_gtk/container.py
@@ -2,16 +2,16 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # License-Filename: LICENSE.md
 
-from gi.repository.Gtk import Builder, Overlay
+from gi.repository import Gtk
 
 from briar_gtk.define import RESOURCES_DIR
 
 
-class Container(Overlay):
+class Container(Gtk.Overlay):
 
     def __init__(self):
         super().__init__()
-        self.builder = Builder()
+        self.builder = Gtk.Builder()
 
     def _add_from_resource(self, ui_filename):
         self.builder.add_from_resource(RESOURCES_DIR + ui_filename)

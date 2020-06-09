62b7cd60bfae61df6465660aec35ee0bbb72e954
Nico Alt <nicoalt@posteo.org>
2020-06-09 13:00:01 +0000

    Add test for Container
    Related to https://code.briarproject.org/briar/briar-gtk/-/issues/26

---
+++ briar-gtk/tests/briar_gtk/test_container.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2020 Nico Alt
+# SPDX-License-Identifier: AGPL-3.0-only
+# License-Filename: LICENSE.md
+
+import pytest
+
+from briar_gtk.container import Container
+
+MODULE = "briar_gtk.container.%s"
+
+
+def test_builder_at_init(mocker):
+    builder_mock = mocker.patch(
+        MODULE % "Gtk.Builder")
+
+    Container()
+
+    builder_mock.assert_called_once()
+
+
+@pytest.fixture(autouse=True)
+def gi_dependencies(mocker):
+    mocker.patch(MODULE % "Gtk")

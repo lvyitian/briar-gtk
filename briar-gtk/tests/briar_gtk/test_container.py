a68c40f5d8dff0709ab68877eed33feffa97ba31
Nico Alt <nicoalt@posteo.org>
2020-06-09 14:00:02 +0000

    Don't execute container test on GitLab CI
    Related to https://code.briarproject.org/briar/briar-gtk/-/issues/2.

--- briar-gtk/tests/briar_gtk/test_container.py
+++ briar-gtk/tests/briar_gtk/test_container.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # License-Filename: LICENSE.md
 
+import os
+
 import pytest
 
 from briar_gtk.container import Container
@@ -9,6 +11,7 @@ from briar_gtk.container import Container
 MODULE = "briar_gtk.container.%s"
 
 
+@pytest.mark.skipif('CI' in os.environ, reason='segmentation fault')
 def test_builder_at_init(mocker):
     builder_mock = mocker.patch(
         MODULE % "Gtk.Builder")

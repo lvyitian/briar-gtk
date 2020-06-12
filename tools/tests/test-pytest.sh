6b39784ae46f653893d40cf447e88776599d347e
Nico Alt <nicoalt@posteo.org>
2020-06-12 12:00:00 +0000

    Include missing lines in coverage report

--- tools/tests/test-pytest.sh
+++ tools/tests/test-pytest.sh
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # License-Filename: LICENSE.md
 
-PYTHONPATH=briar-gtk/briar_gtk pytest --cov=briar_gtk briar-gtk/tests/
+PYTHONPATH=briar-gtk/briar_gtk pytest --cov-report term-missing --cov=briar_gtk briar-gtk/tests/

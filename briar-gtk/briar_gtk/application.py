502705f4c39c7fafeff03825cbc9d47a515c153d
Nico Alt <nicoalt@posteo.org>
2020-09-01 14:00:00 +0000

    Migrate to libhandy 1
    As of August 7, [a first beta release is available](https://mamot.fr/@KekunPlazas/104648142113069390).
    
    The code to build can now be downloaded from [gnome.org](https://download.gnome.org/sources/libhandy/0.90/).
    
    A [migration guide](https://gnome.pages.gitlab.gnome.org/libhandy/hdy-migrating-0-0-to-1.html) is available, just as [packages for Debian](https://packages.debian.org/bullseye/libhandy-1-0).
    
    Fixes #59.

--- briar-gtk/briar_gtk/application.py
+++ briar-gtk/briar_gtk/application.py
@@ -9,7 +9,7 @@
 import gi
 gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
-gi.require_version('Handy', '0.0')
+gi.require_version('Handy', '1')
 from gi.repository import Gdk, Gio, GLib, Gtk, Handy
 
 from briar_wrapper.api import Api

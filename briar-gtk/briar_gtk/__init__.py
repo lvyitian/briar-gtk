5a9ff5cec6f4a609420b2c613691c43e02890106
Nico Alt <nicoalt@posteo.org>
2020-12-17 12:36:54 +0100

    Add __init__ files for modules
    Although it isn't necessary for Python 3, some tools analyzing code
    (e.g. pyreverse from pylint) need those files.

---
+++ briar-gtk/briar_gtk/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2020 Nico Alt
+# SPDX-License-Identifier: AGPL-3.0-only
+# License-Filename: LICENSE.md
+"""
+A GTK application for Briar
+"""
---
+++ briar-gtk/briar_gtk/actions/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2020 Nico Alt
+# SPDX-License-Identifier: AGPL-3.0-only
+# License-Filename: LICENSE.md
+"""
+GTK Actions of Briar GTK
+"""

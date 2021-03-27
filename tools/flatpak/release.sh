1028905133d62b83677bda9876f29914679bc5ac
Nico Alt <nicoalt@posteo.org>
2021-03-27 12:00:00 +0000

    Build flatpaks for aarch64 and arm

--- tools/flatpak/release.sh
+++ tools/flatpak/release.sh
@@ -9,6 +9,13 @@ gpgfile=""
 
 set -e -x
 
-flatpak-builder --repo=$codedir/flatpak-repo --install-deps-from=flathub --user --force-clean --ccache $codedir/flatpak-builddir $codedir/app.briar.gtk.json
+flatpak-builder --arch="x86_64" --disable-cache --repo=$codedir/flatpak-repo --install-deps-from=flathub --user --force-clean --ccache $codedir/flatpak-builddir $codedir/app.briar.gtk.json
 flatpak build-commit-from --src-repo=$codedir/flatpak-repo --gpg-sign=3D7EA4950100A0D222C970717C23232F3BF374D7 --update-appstream --no-update-summary $repodir
+
+flatpak-builder --arch="aarch64" --disable-cache --repo=$codedir/flatpak-repo --install-deps-from=flathub --user --force-clean --ccache $codedir/flatpak-builddir $codedir/app.briar.gtk.json
+flatpak build-commit-from --src-repo=$codedir/flatpak-repo --gpg-sign=3D7EA4950100A0D222C970717C23232F3BF374D7 --update-appstream --no-update-summary $repodir
+
+flatpak-builder --arch="arm" --disable-cache --repo=$codedir/flatpak-repo --install-deps-from=flathub --user --force-clean --ccache $codedir/flatpak-builddir $codedir/app.briar.gtk.json
+flatpak build-commit-from --src-repo=$codedir/flatpak-repo --gpg-sign=3D7EA4950100A0D222C970717C23232F3BF374D7 --update-appstream --no-update-summary $repodir
+
 flatpak build-update-repo --default-branch=stable --gpg-import=$gpgfile --gpg-sign=3D7EA4950100A0D222C970717C23232F3BF374D7 --generate-static-deltas $repodir
